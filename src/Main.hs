941a94ad669c597d35d9e55c80602c7156050a89
vi <me@vikramverma.com>
2014-12-25 22:16:05 +0000

    Unconditionally disable Javascript execution.

--- src/Main.hs
+++ src/Main.hs
@@ -10,10 +10,18 @@ import Control.Monad.Trans.Maybe (MaybeT(..))
 import Data.HTTPSEverywhere.Rules (rewriteURL)
 import Graphics.UI.Gtk (containerAdd, initGUI, mainGUI, mainQuit, onDestroy, widgetShowAll, windowNew, on)
 import Graphics.UI.Gtk.WebKit.NetworkRequest (networkRequestGetUri, networkRequestSetUri)
-import Graphics.UI.Gtk.WebKit.WebView (WebView, webViewNew, webViewLoadUri, resourceRequestStarting)
+import Graphics.UI.Gtk.WebKit.WebSettings (webSettingsEnableScripts)
+import Graphics.UI.Gtk.WebKit.WebView (WebView, webViewNew, webViewLoadUri, resourceRequestStarting, webViewGetWebSettings, webViewSetWebSettings)
 import Network.URI (parseURI)
 import System.Environment (getArgs)
 import System.Glib.Signals (ConnectId)
+import System.Glib.Attributes (AttrOp((:=)), set)
+
+noScript :: WebView -> IO ()
+noScript view = do
+  settings <- webViewGetWebSettings view
+  set settings [webSettingsEnableScripts := False]
+  webViewSetWebSettings view settings
 
 httpsEverywhere :: WebView -> IO (ConnectId WebView)
 httpsEverywhere = flip (flip on resourceRequestStarting) $ \_ _ requestM _ -> void . runMaybeT $ do
@@ -26,5 +34,6 @@ main = bracket_ initGUI mainGUI . void $ do
   (window, view) <- liftM2 (,) windowNew webViewNew
   window `containerAdd` view
   _ <- httpsEverywhere view
+  _ <- noScript view
   getArgs >>= webViewLoadUri view . head
   widgetShowAll window >> onDestroy window mainQuit
