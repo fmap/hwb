9598a0b0c92dcea56c8e009f34e911973599cbc8
vi <me@vikramverma.com>
2014-12-25 23:22:04 +0000

    Set window title to <title>.

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main (main) where
 
 import Control.Error.Util (hoistMaybe)
@@ -9,7 +11,8 @@ import Data.HTTPSEverywhere.Rules (rewriteURL)
 import Graphics.UI.Gtk (containerAdd, initGUI, mainGUI, mainQuit, onDestroy, widgetShowAll, windowNew, on)
 import Graphics.UI.Gtk.WebKit.NetworkRequest (networkRequestGetUri, networkRequestSetUri)
 import Graphics.UI.Gtk.WebKit.WebSettings (WebSettings, webSettingsEnableScripts, webSettingsEnablePrivateBrowsing)
-import Graphics.UI.Gtk.WebKit.WebView (WebView, webViewNew, webViewLoadUri, resourceRequestStarting, webViewGetWebSettings, webViewSetWebSettings)
+import Graphics.UI.Gtk.Windows.Window (Window, windowTitle)
+import Graphics.UI.Gtk.WebKit.WebView (WebView, webViewNew, webViewLoadUri, resourceRequestStarting, webViewGetWebSettings, webViewSetWebSettings, titleChanged)
 import Network.URI (parseURI)
 import System.Environment (getArgs)
 import System.Glib.Attributes (ReadWriteAttr, AttrOp((:=)), set)
@@ -32,6 +35,9 @@ httpsEverywhere = fmap void . flip (flip on resourceRequestStarting) $ \_ _ requ
   httpsURI <- MaybeT (networkRequestGetUri request) >>= hoistMaybe . parseURI >>= liftIO . rewriteURL
   liftIO . networkRequestSetUri request $ show httpsURI
 
+labelWindow :: Window -> Rule
+labelWindow window = fmap void . flip (flip on titleChanged) $ \_ (title :: String) -> set window [ windowTitle := title ]
+
 applyRules :: WebView -> [Rule] -> IO ()
 applyRules view = sequence_ . map ($ view)
 
@@ -43,6 +49,7 @@ main = bracket_ initGUI mainGUI . void $ do
     , httpsEverywhere
     , noScript
     , privateBrowsing
+    , labelWindow window
     ]
   getArgs >>= webViewLoadUri view . head
   widgetShowAll window >> onDestroy window mainQuit
