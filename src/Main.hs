5fed04da291bd8eaa6dd44ce75d15f1ce5f57b17
vi <me@vikramverma.com>
2014-12-26 04:49:33 +0000

    Pitiful abstraction.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main (main) where
 
@@ -11,7 +12,7 @@ import Data.HTTPSEverywhere.Rules (rewriteURL)
 import Graphics.UI.Gtk (containerAdd, initGUI, mainGUI, mainQuit, onDestroy, widgetShowAll, scrolledWindowNew, windowNew, keyPressEvent, eventModifier, eventKeyVal)
 import Graphics.UI.Gtk.Misc.Adjustment (adjustmentGetValue, adjustmentSetValue, adjustmentGetStepIncrement)
 import Graphics.UI.Gtk.WebKit.NetworkRequest (networkRequestGetUri, networkRequestSetUri)
-import Graphics.UI.Gtk.Scrolling.ScrolledWindow (ScrolledWindowClass, scrolledWindowGetVAdjustment)
+import Graphics.UI.Gtk.Scrolling.ScrolledWindow (ScrolledWindowClass, ScrolledWindow, scrolledWindowGetVAdjustment)
 import Graphics.UI.Gtk.WebKit.WebSettings (WebSettings, webSettingsEnableScripts, webSettingsEnablePrivateBrowsing)
 import Graphics.UI.Gtk.WebKit.WebView (WebView, webViewNew, webViewLoadUri, resourceRequestStarting, webViewGetWebSettings, webViewSetWebSettings, titleChanged)
 import Graphics.UI.Gtk.Windows.Window (Window, windowTitle)
@@ -20,29 +21,25 @@ import System.Environment (getArgs)
 import System.Glib.Attributes (ReadWriteAttr, AttrOp((:=)), set)
 import System.Glib.Signals (on, after)
 
-type Rule = WebView -> IO ()
-
 applySettingG :: a -> WebView -> ReadWriteAttr WebSettings a a -> IO ()
 applySettingG assignment view = bracket (webViewGetWebSettings view) (webViewSetWebSettings view) . flip set . return . (:= assignment)
 
-setView, unsetView :: ReadWriteAttr WebSettings Bool Bool -> Rule
+setView, unsetView :: ReadWriteAttr WebSettings Bool Bool -> WebView -> IO ()
 setView = flip $ applySettingG True; unsetView = flip $ applySettingG False
 
-noScript, privateBrowsing :: Rule
-noScript = unsetView webSettingsEnableScripts
-privateBrowsing = setView webSettingsEnablePrivateBrowsing
+noScript, privateBrowsing :: UI -> IO ()
+noScript = unsetView webSettingsEnableScripts . uiWebView
+privateBrowsing = setView webSettingsEnablePrivateBrowsing . uiWebView
 
-httpsEverywhere :: Rule
-httpsEverywhere = fmap void . flip (flip on resourceRequestStarting) $ \_ _ requestM _ -> void . runMaybeT $ do
+httpsEverywhere :: UI -> IO ()
+httpsEverywhere UI{..} = void . on uiWebView resourceRequestStarting $ \_ _ requestM _ -> void . runMaybeT $ do
   request  <- hoistMaybe requestM
   httpsURI <- MaybeT (networkRequestGetUri request) >>= hoistMaybe . parseURI >>= liftIO . rewriteURL
   liftIO . networkRequestSetUri request $ show httpsURI
 
-labelWindow :: Window -> Rule
-labelWindow window = fmap void . flip (flip on titleChanged) $ \_ (title :: String) -> set window [ windowTitle := title ]
-
-applyRules :: WebView -> [Rule] -> IO ()
-applyRules view = sequence_ . map ($ view)
+labelWindow :: UI -> IO ()
+labelWindow UI{..} = void . on uiWebView titleChanged $ \_ (title :: String) -> do
+  set uiWindow [ windowTitle := title ]
 
 scrollW :: ScrolledWindowClass self => self -> (Double -> Double -> Double) -> IO ()
 scrollW window binOp = do
@@ -51,26 +48,36 @@ scrollW window binOp = do
   increment <- adjustmentGetStepIncrement adjustment
   adjustmentSetValue adjustment $ position `binOp` increment
 
-jk :: ScrolledWindowClass self => self -> Rule
-jk window = fmap void . flip (flip after keyPressEvent) $ do
+jk :: UI -> IO ()
+jk UI{..} = void . after uiWebView keyPressEvent $ do
   (k, m) <- liftM2 (,) eventKeyVal eventModifier
   liftIO . unless (m /= []) $ case k of
-    106 -> window `scrollW` (+)
-    107 -> window `scrollW` (-)
+    106 -> uiScrolledWindow `scrollW` (+)
+    107 -> uiScrolledWindow `scrollW` (-)
     _   -> return ()
   return False
 
+data UI = UI
+  { uiWindow         :: Window
+  , uiScrolledWindow :: ScrolledWindow
+  , uiWebView        :: WebView
+  }
+
+withUI :: (UI -> IO a) -> IO ()
+withUI callback = bracket_ initGUI mainGUI . void $ do
+  ui@UI{..} <- liftM3 UI windowNew (scrolledWindowNew Nothing Nothing) webViewNew
+  uiWindow `containerAdd` uiScrolledWindow
+  uiScrolledWindow `containerAdd` uiWebView
+  _ <- callback ui
+  widgetShowAll uiWindow >> onDestroy uiWindow mainQuit
+
 main :: IO ()
-main = bracket_ initGUI mainGUI . void $ do
-  (window, view, scroll) <- liftM3 (,,) windowNew webViewNew (scrolledWindowNew Nothing Nothing)
-  window `containerAdd` scroll
-  applyRules view $ 
-    [ containerAdd scroll
-    , httpsEverywhere
+main = withUI $ \ui@UI{..} -> do
+  sequence_ . map ($ ui) $ 
+    [ httpsEverywhere
     , noScript
     , privateBrowsing
-    , labelWindow window
-    , jk scroll
+    , labelWindow
+    , jk
     ]
-  getArgs >>= webViewLoadUri view . head
-  widgetShowAll window >> onDestroy window mainQuit
+  getArgs >>= webViewLoadUri uiWebView . head
