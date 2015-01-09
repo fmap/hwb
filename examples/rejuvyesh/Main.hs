fe9aac1f744b2b7866fecc1144f8ce762e2f3f07
rejuvyesh <mail@rejuvyesh.com>
2015-01-09 16:08:05 +0530

    enable going back and forward
    enable reloading
    
    Use my (rejuvyesh's) own config;

---
+++ examples/rejuvyesh/Main.hs
@@ -0,0 +1,41 @@
+module Main (
+  main
+) where
+
+import Control.Monad.IO.Class (liftIO)
+import System.Environment (getArgs)
+
+import Graphics.UI.Gtk.Abstract.Widget.Concrete (keyPressEvent) 
+import Graphics.UI.Gtk.WebKit.WebSettings.Concrete (webSettingsEnableScripts, webSettingsEnablePrivateBrowsing)
+import Graphics.UI.Gtk.WebKit.WebView.Concrete (titleChanged, resourceRequestStarting)
+
+import HWB.Core (calls, is)
+import HWB.Main (hwb)
+import HWB.Plugin.Clipboard (yankCurrentURL)
+import HWB.Plugin.HTTPSEverywhere (tryHTTPS)
+import HWB.Plugin.Keybinding (Keybinding(..), tryKeybindings)
+import HWB.Plugin.Navigation (setURL, goBack, goForward, reload, reloadBypassCache)
+import HWB.Plugin.Scrolling (scrollTop, scrollBottom, pageDown, pageUp)
+import HWB.Plugin.WindowTitle (setWindowTitle)
+
+keys :: [Keybinding]
+keys =
+  [ "gg" :== scrollTop
+  , "j"  :== pageDown
+  , "k"  :== pageUp
+  , "G"  :== scrollBottom
+  , "yy" :== yankCurrentURL
+  , "r"  :== reload
+  , "R"  :== reloadBypassCache
+  , "h"  :== goBack
+  , "H"  :== goForward
+  ]
+
+main :: IO ()
+main = hwb $ do
+  webSettingsEnableScripts `is` False
+  webSettingsEnablePrivateBrowsing `is` True
+  resourceRequestStarting `calls` tryHTTPS
+  titleChanged `calls` setWindowTitle
+  keyPressEvent `calls` tryKeybindings keys
+  liftIO getArgs >>= setURL . head
--- src/HWB/Plugin/Navigation.hs
+++ src/HWB/Plugin/Navigation.hs
@@ -1,11 +1,13 @@
 module HWB.Plugin.Navigation (
   setURL,
-  getURL
+  getURL,
+  goBack,
+  goForward
 ) where
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Reader (asks)
-import Graphics.UI.Gtk.WebKit.WebView (webViewLoadUri, webViewGetUri)
+import Graphics.UI.Gtk.WebKit.WebView (webViewLoadUri, webViewGetUri, webViewGoBack, webViewGoForward, webViewReload, webViewReloadBypassCache)
 
 import HWB.Core (H)
 import HWB.UserInterface (UserInterface(userInterfaceWebView))
@@ -15,3 +17,15 @@ setURL url = asks userInterfaceWebView >>= liftIO . flip webViewLoadUri url
 
 getURL :: H (Maybe String)
 getURL = asks userInterfaceWebView >>= liftIO . webViewGetUri
+
+goBack :: H ()
+goBack = asks userInterfaceWebView >>= liftIO . webViewGoBack
+
+goForward :: H ()
+goForward = asks userInterfaceWebView >>= liftIO . webViewGoForward
+
+reload :: H ()
+reload = asks userInterfaceWebView >>= liftIO . webViewReload
+
+reloadBypassCache :: H()
+reloadBypassCache = asks userInterfaceWebView >>= liftIO . webViewReloadBypassCache
